6f10c2475b2de08d6ab5167c6262f4e023fc738e
Mitchy <MitchoppDEV@gmail.com>
2025-10-23 21:19:18 +0100

    h1'd the title and made a the main readme using the tool for fun

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ print(file_name)
 # https://www.w3schools.com/python/python_file_write.asp
 with open(f"{file_name}.md", "w") as file:
     file.write(f"""
-{title}
+# {title}
 ---
 {description}
 ---
