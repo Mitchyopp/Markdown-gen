d03beaccdc89a9fa5969e49633751515e2a282bc
Mitchy <MitchoppDEV@gmail.com>
2025-10-23 20:42:05 +0100

    setup the import and started making structure, im using the newer package instead

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-title = input("Title: \n")
-description = input("description: \n")
-installation = input("installation: \n")
-usage = input("usage: \n")
-liscence = input("liscence: \n")
-author = input("author: \n")
-contact_info = input("contact_info: \n")
+from InquirerPy import prompt
+# I'm using the package below because its the more updated version and i found better documentation on it.
+# https://inquirerpy.readthedocs.io/en/latest/
+
+questions = [
+        { "type": "input", "message": "README title:" },
+        { "type": "input", "message": "README description:" },
+]
+
+result = prompt(questions)
+title = result[0]
+description = result[1]
+print(title)
+print(description)
