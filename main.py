53f880cc5cc0c67e4cc5744472f473e27e441a41
Mitchy <MitchoppDEV@gmail.com>
2025-10-23 20:44:37 +0100

    added all the prompts for the readme)

--- main.py
+++ main.py
@@ -5,10 +5,25 @@ from InquirerPy import prompt
 questions = [
         { "type": "input", "message": "README title:" },
         { "type": "input", "message": "README description:" },
+        { "type": "input", "message": "README installation:" },
+        { "type": "input", "message": "README usage:" },
+        { "type": "input", "message": "README lisence:" },
+        { "type": "input", "message": "README author:" },
+        { "type": "input", "message": "README contact info:" },
 ]
 
 result = prompt(questions)
 title = result[0]
 description = result[1]
+installation = result[2]
+usage = result[3]
+lisence = result[4]
+author = result[5]
+contact = result[6]
 print(title)
 print(description)
+print(installation)
+print(usage)
+print(lisence)
+print(author)
+print(contact)
